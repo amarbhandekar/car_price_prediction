23c174a05cf68d73ef10e5a30105813ec3fef4c8
amarbhandekar <amarbhandekar3142@gmail.com>
2024-09-19 22:11:09 +0530

    modify config file

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 FLASK_PORT_NUMBER=5004
 
-CSV_FILE_NAME=r"data\car_data.csv"
+CSV_FILE_NAME=r"data/car_data.csv"
 
-MODEL_FILE_NAME=r"artifacts\linear_reg_model.pkl"
+MODEL_FILE_NAME=r"artifacts/linear_reg_model.pkl"
 
-COLUMN_DATA_JSON_FILE=r"artifacts\column_data.json"
+COLUMN_DATA_JSON_FILE=r"artifacts/column_data.json"
